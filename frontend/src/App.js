2426a03f0cbc8be3639d61e64cb7901482b8441e
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2023-04-27 15:05:04 +0200

    Eksamen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
 import './App.css';
+import './_examvariables.scss'
+import { store, mygames } from './games';
 
 function App() {
   return (
-<h2>Overskrift</h2>
+    <div>
+      <img src="./macslogo_black.svg" alt="My logo" />
+      <h1>My Game Collection</h1>
+      <ul>
+        {mygames.map((game) => (
+          <li key={game.id}>
+            {game.title} ({game.platform})
+          </li>
+        ))}
+      </ul>
+      <p>Number of games in collection: {store.length}</p>
+    </div>
   );
 }
 
