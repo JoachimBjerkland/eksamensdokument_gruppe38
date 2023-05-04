9b6b270ca3db1dddb1247fbb799f0264992605ef
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2023-05-04 12:15:37 +0200

    Eksamen

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
 import { mygames } from '../games';
 
-function GamePage({  }) {
-  
+function GamePage() {
+  const [game, setGame] = useState(mygames[3]);
+
+  const toggleFavorite = () => {
+    setGame(prevGame => ({ ...prevGame, fav: !prevGame.fav }));
+  };
+
+  return (
+    <div id="gamepage" className="game-card">
+      <h2>{game.title}</h2>
+      <img src={game.img} alt={game.title} />
+      <p><strong>Release Date:</strong> {game.released}</p>
+      <p><strong>Genres:</strong> {game.genres.join(', ')}</p>
+      <p><strong>Game Link:</strong> {game.link}</p>
+      <p><strong>Game Image:</strong> {game.img}</p>
+      <p><strong>Id:</strong> {game.id}</p>
+      <p><strong>Favorite?:</strong> {game.fav ? 'Yes' : 'No'}</p>
+      <a href={game.link} target="_blank" rel="noopener noreferrer">
+        <button className="buy-button">Buy Now</button>
+      </a>
+    </div>
+  );
 }
 
 export default GamePage;
+
 
 
 //Kilder: https://stackoverflow.com/questions/38486660/how-to-add-a-classname-id-to-react-bootstrap-component
