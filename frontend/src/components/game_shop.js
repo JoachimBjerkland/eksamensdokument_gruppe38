5f82285dcfb7645b87f108f8ce586b2e1b298e38
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2023-05-08 12:29:19 +0200

    Eksamen (Api-key lagt til game_shop)

--- frontend/src/components/game_shop.js
+++ frontend/src/components/game_shop.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { store } from '../games';
+import React, { useState, useEffect } from 'react';
 
 function GameShop() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.rawg.io/api/games?key=84ac59c1218a4dc4a60287a81d0a0fbd&dates=2019-09-01,2019-09-30&platforms=18,1,7')
+      .then(res => res.json())
+      .then(data => setGames(data.results.slice(0, 5)))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div>
+    <div id="gameshop">
       <h1 id="gameshop-heading">GAMESHOP</h1>
       <ul>
-        {store.map((game) => (
+        {games.map((game) => (
           <li key={game.id}>
-            <h2>{game.title}</h2>
-            <img src={game.img} alt={game.title} />
-            <p><strong>Release Date:</strong> {game.released}</p>
-            <p><strong>Genres:</strong> {game.genres.join(', ')}</p>
-            <a href={game.link} target="_blank" rel="noopener noreferrer">
-              <button className="buy-button">Buy</button>
-            </a>
+            <img src={game.background_image} alt={game.name} />
+            <div>
+              <h2>{game.name}</h2>
+              <p className="release-date"><strong>Release Date:</strong> {game.released}</p>
+              <p className="genres"><strong>Genres:</strong> {game.genres.map(genre => genre.name).join(', ')}</p>
+              <a href={game.website} target="_blank" rel="noopener noreferrer">
+                <button className="buy-button">Buy</button>
+              </a>
+            </div>
           </li>
         ))}
       </ul>
@@ -23,6 +33,9 @@ function GameShop() {
 }
 
 export default GameShop;
+
+
+
 
 //Kilder: https://stackoverflow.com/questions/38486660/how-to-add-a-classname-id-to-react-bootstrap-component
 //Kilde: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/strong
