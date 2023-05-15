01597650ee08a1bd5aa7e0616d01f8fe5b288976
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2023-05-15 14:44:36 +0200

    Eksamen

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -1,38 +1,42 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function GamePage(props) {
-  const [game, setGame] = useState(null);
-  const { gameId } = props;
+  const [game, setGame, onClose] = useState(null);
 
   useEffect(() => {
-    const fetchGame = async () => {
-      const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=84ac59c1218a4dc4a60287a81d0a0fbd`);
+    const fetchData = async () => {
+      const url = `https://api.rawg.io/api/games/${props.gameId}?key=84ac59c1218a4dc4a60287a81d0a0fbd`;
+      const response = await fetch(url);
       const data = await response.json();
       setGame(data);
     };
-    fetchGame();
-  }, [gameId]);
+    fetchData();
+  }, [props.gameId]);
+
+  const handleClose = () => {
+    setGame(null);
+  }
+
+  if (!game) {
+    return null;
+  }
 
   return (
-    <div className="game-card">
-      {game && (
-        <>
-          <h2>{game.name}</h2>
-          <img src={game.background_image} alt={game.name} />
-          <p><strong>Id:</strong> {game.id}</p>
-          <p><strong>Release Date:</strong> {game.released}</p>
-          <p><strong>Genres:</strong> {game.genres.map(genre => genre.name).join(', ')}</p>
-          <p><strong>Game Link:</strong> <a href={game.website} target="_blank" rel="noopener noreferrer">{game.website}</a></p>
-          <p><strong>Game Image:</strong> {game.background_image}</p>
-          <a href={game.website} target="_blank" rel="noopener noreferrer">
-          </a>
-        </>
-      )}
+    <div id={`game-${game?.id}`} className="game-card">
+      <button onClick={handleClose}>X</button>
+      <h2>{game?.name}</h2>
+      <img id={`game-image-${game?.id}`} src={game?.background_image} alt={game?.name} />
+      <p><strong>Id:</strong> {game?.id}</p>
+      <p><strong>Release Date:</strong> {game?.released}</p>
+      <p><strong>Genres:</strong> {game?.genres?.map(genre => genre.name).join(', ')}</p>
+      <p><strong>Game Link:</strong> <a href={`https://rawg.io/games/${game?.slug}`} target="_blank" rel="noopener noreferrer">{game?.name} on RAWG.io</a></p>
+      <p><strong>Game Image:</strong> {game?.background_image}</p>
     </div>
   );
 }
 
 export default GamePage;
+
 
 
 //Kilder: https://stackoverflow.com/questions/38486660/how-to-add-a-classname-id-to-react-bootstrap-component
